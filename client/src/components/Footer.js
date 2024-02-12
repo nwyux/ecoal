d1d43282ad43acbd210b5840c56a5569a1854b9d
noah <101118381+nwyux@users.noreply.github.com>
2024-02-12 17:05:57 +0100

    tailwind & scss

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer static bottom-0 w-full bg-gray-800 text-white text-center p-4'>
+        footer
+    </div>
+  )
+}
---
+++ client/src/components/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div className='h-[100px] mt-96'>
+        <h1 className='text-3xl'>Hello World</h1>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            Button
+        </button>
+    </div>
+  )
+}
