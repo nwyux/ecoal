c94b2cb32c559e8c3c914b68993dc5eaf3ec10e3
noah <101118381+nwyux@users.noreply.github.com>
2024-02-13 13:37:29 +0100

    navbar login

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,21 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import useCookie from 'react-use-cookie';
+import { NavLink } from 'react-router-dom';
 
 export default function Register() {
-  return (
-<form method='post' action='http://127.0.0.1:8000/api/register'> 
-  <label>
-    Name :
-    <input type="text" name="name" required/>
-  </label>
-  <label>
-    Email :
-    <input type="email" name="email" required/>
-  </label>
-  <label>
-    Password :
-    <input type="text" name="password" required/>
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-  )
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [userToken, setUserToken] = useCookie('token', '0');
+    const [registered, setRegistered ] = useState(false)
+    const navigate = useNavigate()
+
+    async function register(e) {
+        e.preventDefault();
+        setLoading(true);
+        const response = (await axios.post('http://localhost:8000/api/register', {
+            name: name,
+            email: email,
+            password: password,
+        })).data;
+        setLoading(false);
+        setRegistered(true);
+        if (response.error) {
+            setError(response.error);
+        } else {
+            setUserToken(response['access_token'], {
+                days: 365,
+                SameSite: 'Strict',
+                Secure: true,
+            });
+            setRegistered(true);
+        }
+    }
+
+      
+
+
+    if (registered)
+    window.location.href = '/'
+
+    return (
+        <div className="flex h-screen relative justify-center items-center">
+            <div className='flex absolute top-24 z-20 justify-center bg-vert rounded-full items-center mb-4'>
+                <NavLink to='/login' className="text-2xl font-semibold text-vertfonce rounded-full py-4 px-11 ">Login</NavLink>
+                <NavLink to='/register' className="text-2xl font-semibold py-4 px-6 bg-vertfonce text-blanc rounded-full">Register</NavLink>
+              </div>
+            <div className="w-full p-4 bg-blanc absolute top-24 h-screen rounded-3xl">
+                <form onSubmit={register} className='mt-24'>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        className="w-full p-2 mb-6 bg-vert text-vertfonce rounded-xl placeholder:text-vertfonce placeholder:text-xl placeholder:font-bold"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full p-2 mb-6 bg-vert text-vertfonce rounded-xl placeholder:text-vertfonce placeholder:text-xl placeholder:font-bold"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full p-2 mb-6 bg-vert text-vertfonce rounded-xl placeholder:text-vertfonce placeholder:text-xl placeholder:font-bold"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button
+                    className="w-full p-2 bg-vertfonce text-blanc rounded-xl font-bold"
+                    onClick={register}
+                        disabled={loading}
+                    >
+                        Register
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
 }
